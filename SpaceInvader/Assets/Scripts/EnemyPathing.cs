de5d358387f95c1145c4f706c8a120beb35b10d5
Max Gomes <maxgomes92@live.com>
2020-01-01 20:08:09 +0100

    SpaceInvader - WaveConfig for path and speed

---
+++ SpaceInvader/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+  WaveConfig waveConfig;
+  List<Transform> waypoints;
+  private int targetWaypointIndex = 0;
+
+  void Start() {
+    waypoints = waveConfig.GetWaypoints();
+    transform.position = waypoints[targetWaypointIndex].transform.position;
+  }
+
+  void Update() {
+    Move();
+  }
+
+  public void SetWaveConfig(WaveConfig waveConfig) {
+    this.waveConfig = waveConfig;
+  }
+
+  private void Move() {
+    if (targetWaypointIndex < waypoints.Count) {
+      var step = waveConfig.GetMoveSpeed() * Time.deltaTime;
+      var target = waypoints[targetWaypointIndex];
+
+
+      var destiny = Vector2.MoveTowards(
+        transform.position,
+        target.position,
+        step
+      );
+
+      transform.position = destiny;
+
+      if (Equals(transform.position, target.position)) {
+        targetWaypointIndex++;
+      }
+    } else {
+      Destroy(gameObject);
+    }
+  }
+}
---
+++ SpaceInvader/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+  [SerializeField] List<WaveConfig> waveConfigs;
+
+  private int startingWave = 1;
+  void Start() {
+    var currentWave = waveConfigs[startingWave];
+    StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+  }
+
+  private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig) {
+    var numberOfWaves = waveConfigs.Count;
+
+    for (int i = 0; i < waveConfig.GetNumberOfEnemies(); i++) {
+      var enemy = Instantiate(
+        waveConfig.GetEnemyPrefab(),
+        waveConfig.GetWaypoints()[0].transform.position,
+        Quaternion.identity
+      );
+
+      enemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+
+      yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+    }
+
+    startingWave++;
+    if (startingWave == numberOfWaves) {
+      startingWave = 0;
+      Start();
+    }
+  }
+}
