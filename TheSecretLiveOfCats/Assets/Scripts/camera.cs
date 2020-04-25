bd4f42c362cce4173f151252259063685b15a6bf
Max Gomes <maxgomes92@live.com>
2020-04-25 22:14:55 +0200

    Add sprites to cats game

---
+++ TheSecretLiveOfCats/Assets/Scripts/camera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+  [SerializeField] GameObject player = null;
+
+  float offset = 3f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+        
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    transform.position = new Vector3(
+      player.transform.position.x + offset,
+      transform.position.y,
+      transform.position.z
+    );
+  }
+}
---
+++ TheSecretLiveOfCats/Assets/Scripts/player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+  Animator animator;
+  Rigidbody2D rigidBody;
+  [SerializeField] float speed = 300f;
+
+  private bool playerFacingRight = true;
+  void Start() {
+    rigidBody = transform.GetComponent<Rigidbody2D>();
+    animator = transform.GetComponent<Animator>();
+  }
+
+  
+  void Update() {
+    Move();
+  }
+
+  void Move() {
+    Vector2 velocity = rigidBody.velocity;
+    animator.SetFloat("speed", Mathf.Abs(velocity.x));
+
+    if (Input.GetKey(KeyCode.RightArrow)) {
+      velocity.x = speed * Time.deltaTime;
+      transform.localRotation = Quaternion.Euler(0, 0, 0);
+    } else if (Input.GetKey(KeyCode.LeftArrow)) {
+      velocity.x = -speed * Time.deltaTime;
+      transform.localRotation = Quaternion.Euler(0, 180, 0);
+    } else {
+      velocity.x = 0;
+    }
+
+    rigidBody.velocity = velocity;
+  }
+
+  private void Flip() {
+    // Switch the way the player is labelled as facing.
+    playerFacingRight = !playerFacingRight;
+
+    // Multiply the player's x local scale by -1.
+    Vector3 theScale = transform.localScale;
+    theScale.x *= -1;
+    transform.localScale = theScale;
+  }
+}
