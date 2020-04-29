c32e088e201e9d76936eb689c480798c7bf8992a
Max Gomes <maxgomes92@live.com>
2020-04-29 20:38:18 +0200

    Implement isGrounded

--- TheSecretLiveOfCats/Assets/Scripts/player.cs
+++ TheSecretLiveOfCats/Assets/Scripts/player.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+  bool isGrounded = true;
+  bool isFacingRight = true;
   Animator animator;
   Rigidbody2D rigidBody;
   [SerializeField] float speed = 300f;
+  [SerializeField] float jumpForce = 500f;
+  [SerializeField] LayerMask groundLayers;
 
   void Start() {
     rigidBody = transform.GetComponent<Rigidbody2D>();
@@ -14,23 +18,65 @@ public class Player : MonoBehaviour
   }
 
   void Update() {
+    isGrounded = Physics2D.OverlapArea(
+      new Vector2(transform.position.x - 1.5f, transform.position.y - 1.3f),
+      new Vector2(transform.position.x + 1.5f, transform.position.y - 1.3f),
+      groundLayers
+    );
+
+    // Update animator parameters
+    Vector2 velocity = rigidBody.velocity;
+    animator.SetFloat("speed", Mathf.Abs(velocity.x));
+
     Move();
+    Jump();
+  }
+
+  void Jump() {
+    if (isGrounded && Input.GetKey(KeyCode.Space)) {
+      animator.SetBool("isPreJumping", true);
+    } else if (isGrounded && Input.GetKeyUp(KeyCode.Space)) {
+      animator.SetBool("isPreJumping", false);
+      animator.SetBool("isJumping", true);
+      rigidBody.AddForce(new Vector2(0f, jumpForce));
+    } else if (isGrounded) {
+      animator.SetBool("isJumping", false);
+    }
   }
 
   void Move() {
     Vector2 velocity = rigidBody.velocity;
-    animator.SetFloat("speed", Mathf.Abs(velocity.x));
 
-    if (Input.GetKey(KeyCode.RightArrow)) {
+    if (animator.GetBool("isPreJumping")) {
+      velocity.x = 0;
+    } else if (Input.GetKey(KeyCode.RightArrow)) {
       velocity.x = speed * Time.deltaTime;
-      transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+      if (!isFacingRight) {
+        transform.Rotate(0, 180, 0, Space.Self);
+      }
+
+      isFacingRight = true;
     } else if (Input.GetKey(KeyCode.LeftArrow)) {
       velocity.x = -speed * Time.deltaTime;
-      transform.localRotation = Quaternion.Euler(0, 180, 0);
+
+      if (isFacingRight) {
+        transform.Rotate(0, 180, 0, Space.Self);
+      }
+
+      isFacingRight = false;
     } else {
       velocity.x = 0;
-    }
+    }    
 
     rigidBody.velocity = velocity;
   }
+
+  //private void OnDrawGizmos() {
+  //  Gizmos.color = new Color(0, 1, 0, 1);
+  //  Gizmos.DrawCube(
+  //    new Vector2(transform.position.x, transform.position.y - 1.3f),
+  //    new Vector2(3, 0.1f)
+  //  );
+  //}
 }
